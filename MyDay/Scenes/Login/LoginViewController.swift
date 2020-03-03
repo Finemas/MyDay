226071ed642ce4f018f82ed2394f3498d8a8f511
Jan Provazník <proovaznik@gmail.com>
2020-03-03 20:49:24 +0100

    Add login forms

--- MyDay/Scenes/Login/LoginViewController.swift
+++ MyDay/Scenes/Login/LoginViewController.swift
@@ -7,16 +7,52 @@
 //
 
 import UIKit
+import SnapKit
 
 class LoginViewController: UIViewController {
-    @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    func setupView() {
+        view.addSubview(formStackView)
+        formStackView.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+        
+        formStackView.addArrangedSubview(usernameTextField)
+        formStackView.addArrangedSubview(passwordTextField)
+//        formStackView.addArrangedSubview(loginButton)
+    }
+    
+    private lazy var formStackView: UIStackView = {
+        let formStackView = UIStackView()
+        formStackView.alignment = .center
+        formStackView.axis = .vertical
+        formStackView.distribution = .fill
+        formStackView.spacing = 20
+        return formStackView
+    }()
+    
+    private lazy var usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.textContentType = .username
+        textField.placeholder = "username"
+        return textField
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.textContentType = .password
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+//    private lazy var loginButton: UIButton = {
+//        let loginButton = UIButton()
+//        loginButton.setTitle("Login", for: .normal)
+//        return loginButton
+//    }()
 }
-
